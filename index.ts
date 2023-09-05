6bbfd55992746c93bd1785ca57e2a0a56ad644c6
gabrielluizsf <gabrielluizads123@gmail.com>
2023-09-05 13:16:05 -0300

    chamando as rotas no index

--- index.ts
+++ index.ts
@@ -1,47 +1,22 @@
-import express,{Request,Response} from "express";
-import { LinkedList } from "./data/linked-list/linkedList.mjs"
-import { BinaryTree } from "./data/binary-search-tree/bst.mjs";
+import express, {Request,Response} from "express";
+import linkedListRoutes from "./routes/linkedList.routes.mjs";
+import binarySearchRoutes from "./routes/bst.routes.mjs";
 
-const app = express();
+const server = express()
 const PORT = process.env.PORT || 3000;
 
-// criando a constante que possui os metódos da classe de exemplo da estrutura de dados LinkedList
-const numberLinkedList = new LinkedList<number>()
-// criando a constante que possui os metódos públicos da classe de exemplo da estrutura de dados BinaryTree
-const nameBinaryTree = new BinaryTree<string>()
 // Middleware para fazer o parse do corpo das requisições em JSON
-app.use(express.json());
+server.use(express.json());
 
 // Rota para ter resposta no endpoint http://localhost:3000/
-app.get('/', (req: Request, res: Response) => {
+server.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Bem-vindo à minha API!' });
 });
 
+server.use(linkedListRoutes)
+server.use(binarySearchRoutes)
 
-app.post('/linkedList/AddNumber',(req: Request, res: Response)=>{
-  const newNumber = req.body.number;
-  numberLinkedList.append(newNumber)
-  res.status(201).json({message:`Número ${newNumber} adicionado com sucesso na lista ligada`})
-})
-
-app.get('/linkedList/GetNumbers',(req: Request, res: Response)=>{
-  const myNumbers = numberLinkedList.display()
-  res.json({numbers:myNumbers})
-})
-
-app.post('/binarySearchTree/AddName',(req: Request, res: Response)=>{
-  const newName = req.body.newName;
-  nameBinaryTree.insert(newName)
-  res.status(201).json({message: `Nome ${newName} adicionado com sucesso na árvore binária`})
-})
-
-app.get('/binarySearchTree/Search/:myName',(req: Request, res: Response)=>{
-  const myName = req.params.myName;
-  const name = nameBinaryTree.search(myName)
-  res.json({myName: name})
-})
-
-// Inicializa o servidor
-app.listen(PORT, () => {
+// Inicializa o servidor http
+server.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
